005fcc897b365f04be0ffa4d3be4c3d1ec091933
Basil Nyachogo <basil.nyachogo@gmail.com>
2015-01-26 16:22:02 -0500

    switching to nt-table ----broken

--- public/core.js
+++ public/core.js
@@ -1,17 +1,7 @@
 
 
-var BlogApp = angular.module('BlogApp', ['ngRoute','blogController', 'userService']);
+var BlogApp = angular.module('BlogApp', ['ngRoute','ngTable','blogControllers', 'userService']);
 
-// BlogApp.config(['$routeProvider','$locationProvider'
-//         function($routeProvider,$locationProvider){
-//           $routeProvider.
-//             when('/users',{
-//               templateUrl: 'partials/users.html',
-//               controller: 'userCtrl'
-//             });
-//
-//             $locationProvider.html5Mode(true);
-//         }])
 BlogApp.config(function($routeProvider, $locationProvider){
     $routeProvider
       .when('/users',{
